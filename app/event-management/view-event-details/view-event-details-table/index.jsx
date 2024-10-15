3f3b94267297c9d983020d3446dc3e2e82af6ed2
Zeet Geek <zeetgeek435@gmail.com>
2024-10-15 16:34:58 +0530

    complete all pages.

--- app/event-management/view-event-details/view-event-details-table/index.jsx
+++ app/event-management/view-event-details/view-event-details-table/index.jsx
@@ -1,7 +1,13 @@
+import Image from "next/image";
+import VolunteerImage from "@/images/user.jpg";
+import style from "./view-event-details-table.module.scss";
+import clsx from "clsx";
+// import Pagination from "@/_components/global/ui/pagination";
+
 const ViewEventDetailsTable = () => {
     return (
         <div className="layout_box no_padding">
-            <table className="w-100">
+            <table className={clsx(style.view_event_details, "w-100")}>
                 <thead>
                     <tr>
                         <th>Volunteer Name</th>
@@ -13,16 +19,114 @@ const ViewEventDetailsTable = () => {
                 <tbody>
                     <tr>
                         <td>
-                            <span>Volunteer 1</span>
+                            <div className={style.volunteer}>
+                                <Image src={VolunteerImage} height={26} width={26} alt="event_image" />
+                                <span>Volunteer 1</span>
+                            </div>
                         </td>
                         <td>
                             <span>7/4/204, 12:00am</span>
                         </td>
-                        <td>United Statest</td>
-                        <td>Lorem ipsum.......</td>
+                        <td>
+                            <span>United Statest</span>
+                        </td>
+                        <td>
+                            <p className={style.description}>
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus quia fugit eveniet
+                                nostrum cumque provident minima quidem expedita voluptates sunt.
+                            </p>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <div className={style.volunteer}>
+                                <Image src={VolunteerImage} height={26} width={26} alt="event_image" />
+                                <span>Volunteer 1</span>
+                            </div>
+                        </td>
+                        <td>
+                            <span>7/4/204, 12:00am</span>
+                        </td>
+                        <td>
+                            <span>United Statest</span>
+                        </td>
+                        <td>
+                            <p className={style.description}>
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus quia fugit eveniet
+                                nostrum cumque provident minima quidem expedita voluptates sunt.
+                            </p>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <div className={style.volunteer}>
+                                <Image src={VolunteerImage} height={26} width={26} alt="event_image" />
+                                <span>Volunteer 1</span>
+                            </div>
+                        </td>
+                        <td>
+                            <span>7/4/204, 12:00am</span>
+                        </td>
+                        <td>
+                            <span>United Statest</span>
+                        </td>
+                        <td>
+                            <p className={style.description}>
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus quia fugit eveniet
+                                nostrum cumque provident minima quidem expedita voluptates sunt.
+                            </p>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <div className={style.volunteer}>
+                                <Image src={VolunteerImage} height={26} width={26} alt="event_image" />
+                                <span>Volunteer 1</span>
+                            </div>
+                        </td>
+                        <td>
+                            <span>7/4/204, 12:00am</span>
+                        </td>
+                        <td>
+                            <span>United Statest</span>
+                        </td>
+                        <td>
+                            <p className={style.description}>
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus quia fugit eveniet
+                                nostrum cumque provident minima quidem expedita voluptates sunt.
+                            </p>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <div className={style.volunteer}>
+                                <Image src={VolunteerImage} height={26} width={26} alt="event_image" />
+                                <span>Volunteer 1</span>
+                            </div>
+                        </td>
+                        <td>
+                            <span>7/4/204, 12:00am</span>
+                        </td>
+                        <td>
+                            <span>United Statest</span>
+                        </td>
+                        <td>
+                            <p className={style.description}>
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus quia fugit eveniet
+                                nostrum cumque provident minima quidem expedita voluptates sunt.
+                            </p>
+                        </td>
                     </tr>
                 </tbody>
             </table>
+            <div className={clsx(style.pagination_wp, "d-flex align-items-center justify-content-between")}>
+                <span>02 of 21</span>
+                {/* <Pagination /> */}
+            </div>
         </div>
     );
 };
